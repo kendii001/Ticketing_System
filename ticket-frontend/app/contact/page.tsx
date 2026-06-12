b42d02b0ba5cac6e9a378a3c2853fe8c223e9666
Ykendii <kendijackline004@gmail.com>
2026-06-12 18:25:14 +0300

    add the changes

---
+++ ticket-frontend/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from '@/components/navbar';
+import Footer from '@/components/footer';
+export default function Contact() {
+  return (
+    <div>
+      <Navbar/>
+      <h1>Contact Us</h1>
+      <p>Welcome to our Contact page.</p>
+<Footer/>
+    </div>
+  );
+}
