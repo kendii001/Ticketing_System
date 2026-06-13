445d65d7b068eefce23c1d3d8ee17be3c5337fe7
Ykendii <kendijackline004@gmail.com>
2026-06-13 22:55:28 +0300

    include the changes

--- ticket-frontend/app/about/page.tsx
+++ ticket-frontend/app/about/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
+import GoogleAnalytics from '@/components/GoogleAnalytics';
 export default function About() {
   return (
     <div>
       <Navbar/>
       <h1>About Us</h1>
+      <GoogleAnalytics />
       <p>Welcome to our About page.</p>
 <Footer/>
     </div>
---
+++ ticket-frontend/app/home/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Ticketing System</h1>
+      <p>This is the home page of our ticketing system. Here you can find information about our services and how to get started.</p>
+    </div>
+  );
+}
